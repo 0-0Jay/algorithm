2233fef13fc0edeb469e27c3441fc2718cfc9a67
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-04-04 18:05:25 +0900

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+char s[1010];
+int stk[1010], top = -1;
+queue<int> q;
+int main() {
+    int answer = -1;
+    scanf("%s", &s);
+    printf("%d", answer);
+}
