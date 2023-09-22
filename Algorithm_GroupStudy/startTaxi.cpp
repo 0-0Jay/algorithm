5b6044c76c28086eb6fe1f12da9c20db2054f813
조영진 <117143398+Jo-0jin@users.noreply.github.com>
2023-09-22 11:32:45 +0900

    Update startTaxi.cpp

--- Algorithm_GroupStudy/startTaxi.cpp
+++ Algorithm_GroupStudy/startTaxi.cpp
@@ -1,5 +1,4 @@
 // 백준 19238번 스타트 택시 : https://www.acmicpc.net/problem/19238
-// !!! 아직 통과하지 못한 코드. 재작성 필수 !!!
 
 #include<stdio.h>
 #include<iostream>
@@ -7,25 +6,21 @@
 #include<queue>
 using namespace std;
 
-struct tmp {
-	int x, y, d;
-
-};
-
-struct p_tmp {
+struct qtmp { int x, y, d; };
+struct pqtmp { 
 	int x, y;
-
-	bool operator<(const p_tmp t) const {
-		if (x < t.x) return true;
-		else if (y < t.y) return true;
-		return false;
+	bool operator<(const pqtmp& t) const {
+		if (x != t.x) {
+			return x > t.x;
+		}
+		return y > t.y;
 	}
 };
 
-int map[21][21], n, m, f, sx, sy, chk[21][21], hit = 500, flag = 1, drive[21][21], use, arrive,
+int n, m, f, map[21][21], pchk[21][21], echk[21][21], sx, sy, flag, hit, endhit,
 dx[4] = { 1, 0, -1, 0 }, dy[4] = { 0, 1, 0, -1 };
-queue<tmp> que;
-priority_queue<p_tmp> pq;
+queue<qtmp> que;
+priority_queue<pqtmp> pq;
 
 int main() {
 	scanf("%d%d%d", &n, &m, &f);
@@ -35,79 +30,104 @@ int main() {
 		}
 	}
 	scanf("%d%d", &sx, &sy);
+	// 출발점, 도착점 저장
+	for (int i = 0; i < m; i++) {
+		int a1, b1, a2, b2;
+		scanf("%d %d %d %d", &a1, &b1, &a2, &b2);
+		map[a1][b1] = a2 * 100 + b2;
+	}
+	// 탐색 시작
 	while (m > 0) {
-		chk[sx][sy] = flag;
-		que = queue<tmp>();
-		pq = priority_queue<p_tmp>();
+		// 체크, 큐, 힙, hit 초기화
+		flag++;
+		hit = 500000; endhit = 500000;
+		que = queue<qtmp>();
+		pq = priority_queue<pqtmp>();
+		
 		que.push({ sx, sy, 0 });
-		for (int i = 0; i < 3; i++) {
-			int a, b, a2, b2, line = 10;
-			scanf("%d%d%d%d", &a, &b, &a2, &b2);
-			map[a][b] = line;  // 사람은 line
-			map[a2][b2] = -line;  // 목적지는 -line
-			line++;
-		}
-		while (!que.empty()) {  // 가장 가깝고 같은 거리에 위치한 사람
-			tmp now = que.front();
+		pchk[sx][sy] = flag;
+		while (!que.empty()) {
+			qtmp now = que.front();
 			que.pop();
-			if (now.d >= f) break;
-			if (now.d == hit) break;  // now가 hit 거리면 탐색 중단
+			if (now.d > hit) break;  // hit보다 크면 break;
+			if (map[now.x][now.y] != 0) {  // 사람을 발견하면 hit, 사람좌표는 pq에 삽입
+				hit = now.d;
+				pq.push({ now.x, now.y });
+			}
 			for (int i = 0; i < 4; i++) {
-				int a = now.x + dx[i];
-				int b = now.y + dy[i];
-				if (a > 0 && a <= n && b > 0 && b <= n && chk[a][b] != flag && map[a][b] == 0) {
-					if (map[a][b] > 10) {
-						pq.push({a, b});
-						hit = now.d + 1;
-					}
-					que.push({a, b, now.d + 1});
-					chk[a][b] = flag;
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				// 범위 안이고, 벽이 아니며, 방문한 적이 없으면
+				if (nx > 0 && nx <= n && ny > 0 && ny <= n && map[nx][ny] != 1 && pchk[nx][ny] != flag) {
+					if (now.d + 1 > hit) continue;
+					que.push({ nx, ny, now.d + 1 });
+					pchk[nx][ny] = flag;
 				}
 			}
 		}
-		f -= hit;
-		if (pq.empty()) {  // 사람이 없으면 -1
+
+		if (hit > f) {  // hit가 남은 연료보다 크면 -1 출력
 			printf("-1");
 			return 0;
 		}
 
-		p_tmp cl = pq.top();
-		int fin = -map[cl.x][cl.y];
-		map[cl.x][cl.y] = 0;
-		sx = cl.x, sy = cl.y;
-		que.push({sx, sy, 0});
-		drive[sx][sy] = flag;
+		// 힙에서 사람 pop
+		sx = pq.top().x;
+		sy = pq.top().y;
+		int finx = map[sx][sy] / 100;
+		int finy = map[sx][sy] % 100;
+		map[sx][sy] = 0;  // 탐색되지 않게 0으로 초기화
+		f -= hit; // 연료 감소
+
+		que = queue<qtmp>();  // 큐 초기화
+		que.push({ sx, sy, 0 });
 		while (!que.empty()) {
-			tmp now = que.front();
+			qtmp now = que.front();
 			que.pop();
-			if (now.d >= f) break;
-			if (map[now.x][now.y] == fin) {
-				arrive = 1;
-				f += now.d;
-				sx = now.x;
+			if (now.x == finx && now.y == finy) {  // 도착 지점에 도착하면 연료 기록하고 break;
+				endhit = now.d;
+				sx = now.x;  // 택시 위치 도착지점으로 변경
 				sy = now.y;
 				break;
 			}
+			// 도착지점 탐색
 			for (int i = 0; i < 4; i++) {
-				int a = now.x + dx[i];
-				int b = now.y + dy[i];
-				if (a > 0 && a <= n && b > 0 && b <= n && drive[a][b] != flag && map[a][b] == 0) {
-					if (map[a][b] > 10) {
-						pq.push({ a, b });
-						hit = now.d + 1;
-					}
-					que.push({ a, b, now.d + 1 });
-					drive[a][b] = flag;
+				int nx = now.x + dx[i];
+				int ny = now.y + dy[i];
+				// 범위 안이고, 벽이 아니며, 방문한 적 없으면
+				if (nx > 0 && nx <= n && ny > 0 && ny <= n && map[nx][ny] != 1 && echk[nx][ny] != flag) {
+					que.push({ nx, ny, now.d + 1 });
+					echk[nx][ny] = flag;
 				}
 			}
 		}
-		if (arrive == 0) {
+
+		if (endhit > f) {  // endhit이 남은 연료보다 크면 -1 출력
 			printf("-1");
 			return 0;
 		}
-		flag++;
+
+		f += endhit;  // 사용한 연료만큼 충전
 		m--;
-		arrive = 0;
 	}
 	printf("%d", f);
 }
+
+// 알고리즘 : BFS (너비 우선 탐색) + 구현
+/*
+풀이 : 한번의 운행당 BFS를 두번(손님을 찾으러 가는 탐색, 손님의 목적지로 가는 탐색) 수행한다.
+각 손님의 좌표에 목적지 좌표를 활용하기 쉽게 x * 100 + y 꼴로 변환하여 삽입
+1. 택시 위치에서 손님 탐색 BFS를 수행한다.
+2. 가장 먼저 탐색되는 손님의 거리를 hit에 두고, hit보다 먼 거리는 탐색하지 않는다.
+  2-1 : 손님이 탐색되지 않거나 hit이 남은 연료보다 크면 -1 출력 후 프로그램 종료
+3. hit 범위 내의 손님들은 priority_queue(이하 pq)에 넣어서 손님 좌표 비교
+  3-1 : x가 작은 순, x가 같다면 y가 작은 순
+4. pq에서 pop한 손님의 좌표로 택시 위치 변경, 남은 연료 - hit
+  4-1: 해당 손님의 좌표는 0으로 초기화하여 중복 탐색 방지
+5. 택시 위치에서 해당 손님의 목적지 탐색 BFS 수행
+6. 손님의 목적지를 찾으면 그 좌표로 택시 위치 변경, 남은 연료 + 이동한 거리
+  6-1: 만약 남은 연료보다 목적지까지의 거리가 멀면 -1 출력 후 프로그램 종료
+7. 손님카운트 1 감소
+8. 손님을 모두 운반할 때까지 1~7까지 반복
+9. 손님을 모두 운반하면 잔여 연료량 출력
+*/
