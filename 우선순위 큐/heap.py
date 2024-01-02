ed66a245042018909b2ff0cdd086f218af596902
조영진 <117143398+0-0Jay@users.noreply.github.com>
2024-01-02 12:30:31 +0900

    Update heap.py

--- 우선순위 큐/heap.py
+++ 우선순위 큐/heap.py
@@ -1,44 +1,37 @@
 # 백준 1927번 최소 힙 : https://www.acmicpc.net/problem/1927
 
 import sys
+input = sys.stdin.readline
 
 def up(k):
-    if k // 2 == 0:
-        return
-    if heap[k // 2] > heap[k]:
-        temp = heap[k//2]
-        heap[k//2] = heap[k]
-        heap[k] = temp
-        up(k//2)
-
+    if k // 2 == 0: return
+    if arr[k] < arr[k // 2]:
+        arr[k // 2], arr[k] = arr[k], arr[k // 2]
+        up(k // 2)
+     
 def down(k):
-    sw = k*2
-    if sw >= len(heap): # 왼쪽 자식이 없으면
-        return
-    if sw + 1 < len(heap) and heap[sw] > heap[sw + 1]: # 오른쪽 자식이 있고 더 작다면 변경
-        sw += 1
-    if heap[k] > heap[sw]:
-        temp = heap[k]
-        heap[k] = heap[sw]
-        heap[sw] = temp
+    sw = k * 2
+    if sw >= len(arr): return
+    if sw + 1 < len(arr) and arr[sw] > arr[sw + 1]: sw += 1
+    if arr[k] > arr[sw]:
+        arr[k], arr[sw] = arr[sw], arr[k]
         down(sw)
-
+        
 n = int(input())
-heap = [0]
-for _ in range(n):
-    num = int(sys.stdin.readline())
+arr = [0]
+for i in range(n):
+    num = int(input())
     if num == 0:
-        if len(heap) == 1:
+        if len(arr) == 1: 
             print(0)
         else:
-            temp = heap[1]
-            heap[1] = heap[-1]
-            heap[-1] = temp
-            print(heap.pop(-1))
+            print(arr[1])
+            arr[1] = arr[-1]
+            arr.pop(-1)
             down(1)
     else:
-        heap.append(num)
-        up(len(heap)-1)
+        arr.append(num)
+        up(len(arr) - 1)
 
 # 알고리즘 : 우선순위 큐
 ''' 
